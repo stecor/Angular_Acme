88cd329197e868be9fc27a7895f69e956e294ab5
Stefano Corra <stefano.corra@hotmail.com>
2017-09-08 19:18:52 -0400

    fourth commit

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -32,7 +32,7 @@ export class ProductListComponent implements OnInit{
             "releaseDate": "March 19, 2016",
             "description": "Leaf rake with 48-inch wooden handle.",
             "price": 19.95,
-            "starRating": 3.2,
+            "starRating": 5,
             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
         },
         {
@@ -42,7 +42,7 @@ export class ProductListComponent implements OnInit{
             "releaseDate": "March 18, 2016",
             "description": "15 gallon capacity rolling garden cart",
             "price": 32.99,
-            "starRating": 4.2,
+            "starRating": 4,
             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
         },
         {
@@ -52,7 +52,7 @@ export class ProductListComponent implements OnInit{
             "releaseDate": "May 21, 2016",
             "description": "Curved claw steel hammer",
             "price": 8.9,
-            "starRating": 4.8,
+            "starRating": 3,
             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
         },
         {
@@ -62,7 +62,7 @@ export class ProductListComponent implements OnInit{
             "releaseDate": "May 15, 2016",
             "description": "15-inch steel blade hand saw",
             "price": 11.55,
-            "starRating": 3.7,
+            "starRating": 2,
             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
         },
         {
@@ -72,7 +72,7 @@ export class ProductListComponent implements OnInit{
             "releaseDate": "October 15, 2015",
             "description": "Standard two-button video game controller",
             "price": 35.95,
-            "starRating": 4.6,
+            "starRating": 1,
             "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
         }
     ];
@@ -82,11 +82,17 @@ export class ProductListComponent implements OnInit{
         this.listFilter = 'cart';
     }
 
+    onRatingClicked(message : string):void{
+        this.pageTitle = 'Product List: ' + message;
+    }
+
     performFilter(filterBy: string): IProduct[]{
         filterBy = filterBy.toLocaleLowerCase();
         return this.products.filter((product: IProduct)=>
                 product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1)
     }
+
+    
 
     toggleImage(): void {
         this.showImage = !this.showImage;
