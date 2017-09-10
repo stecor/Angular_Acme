0a19e92487f7907a141e3c7cfe1ff148ac08011b
Stefano Corra <stefano.corra@hotmail.com>
2017-09-10 18:45:37 -0400

    sixth commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+
 import { ProductService } from './products/product.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers:[ProductService]
+  selector: 'pm-root',
+  template: `
+    <div>
+        <nav class='navbar navbar-default'>
+            <div class='container-fluid'>
+                <a class='navbar-brand'>{{pageTitle}}</a>
+                <ul class='nav navbar-nav'>
+                    <li><a [routerLink]="['/welcome']">Home</a></li>
+                    <li><a [routerLink]="['/products']">Product List</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class='container'>
+            <router-outlet></router-outlet>
+        </div>
+     </div>
+    `,
+    providers: [ ProductService ]
 })
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';
